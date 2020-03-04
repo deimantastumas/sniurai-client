52c204ff1c81c16d18b62493ee03a4cfcb537796
matas <matasspeicys@gmail.com>
2020-03-04 17:10:57 +0200

    Add aws-amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-north-1",
+        BUCKET: "sniurai-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://2rf3qmpy5g.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_AwdeaPECq",
+        APP_CLIENT_ID: "59fn8el75lihge1grjcfbsfgr3",
+        IDENTITY_POOL_ID: "us-east-2:e631fd73-7cc5-4fe8-be79-0413de06f541"
+    }
+};
