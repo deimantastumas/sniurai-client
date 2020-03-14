7b6145ab1e719a27fcb63a0c60c1b04d5de76528
matas <matasspeicys@gmail.com>
2020-03-14 14:29:09 +0200

    add signup

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,23 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from './components/AppliedRoute';
-import Home from "./containers/Home";
-import Login from "./containers/Login";
-import NotFound from "./containers/NotFound";
+import Home from './containers/Home';
+import Login from './containers/Login';
+import NotFound from './containers/NotFound';
+import Signup from './containers/Signup';
 
 export default function Routes({ appProps }) {
-    return (
-        <Switch>
-            <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-            <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-            <Route component={NotFound} />
-        </Switch>
-    );
+  return (
+    <Switch>
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute
+        path="/signup"
+        exact
+        component={Signup}
+        appProps={appProps}
+      />
+      <Route component={NotFound} />
+    </Switch>
+  );
 }
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,62 +1,65 @@
-import { Auth } from "aws-amplify";
-import React, { useState } from "react";
-import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import LoaderButton from "../components/LoaderButton";
-import "./Login.css";
+import { Auth } from 'aws-amplify';
+import React, { useState } from 'react';
+import { ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
+import LoaderButton from '../components/LoaderButton';
+import { useFormFields } from '../libs/hooksLib';
+import './Login.css';
 
 export default function Login(props) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [fields, handleFieldChange] = useFormFields({
+    email: '',
+    password: ''
+  });
+  const [isLoading, setIsLoading] = useState(false);
 
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
+  function validateForm() {
+    return fields.email.length > 0 && fields.password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await Auth.signIn(fields.email, fields.password);
+      props.userHasAuthenticated(true);
+      props.history.push('/');
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
     }
+  }
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-        setIsLoading(true);
-
-        try {
-            await Auth.signIn(email, password);
-            props.userHasAuthenticated(true);
-            props.history.push("/");
-        } catch (e) {
-            alert(e.message);
-            setIsLoading(false);
-        }
-    }
-
-    return (
-        <div className="Login">
-            <form onSubmit={handleSubmit}>
-                <FormGroup controlId="email" bsSize="large">
-                    <ControlLabel>Email</ControlLabel>
-                    <FormControl
-                        autoFocus
-                        type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup controlId="password" bsSize="large">
-                    <ControlLabel>Password</ControlLabel>
-                    <FormControl
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        type="password"
-                    />
-                </FormGroup>
-                <LoaderButton
-                    block
-                    type="submit"
-                    bsSize="large"
-                    isLoading={isLoading}
-                    disabled={!validateForm()}
-                    >
-                    Login
-                </LoaderButton>
-            </form>
-        </div>
-    );
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={fields.password}
+            onChange={handleFieldChange}
+            type="password"
+          />
+        </FormGroup>
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Login
+        </LoaderButton>
+      </form>
+    </div>
+  );
 }
